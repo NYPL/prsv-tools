9f9553616bb1a63eb4484b43698c73882dc91e31
Nick Krabbenhoeft <nickkrabbenhoeft@nypl.org>
2022-11-04 13:54:12 -0400

    functional tests for er linter

---
+++ tests/test_lint_er.py
@@ -0,0 +1,12 @@
+import bin.lint_er as lint_er
+
+def test_lint_valid_package():
+    """Run entire script with valid ER"""
+    lint_er.main()
+    assert False
+
+def test_lint_invalid_package():
+    """Run entire script with valid ER"""
+    lint_er.main()
+    assert False
+
