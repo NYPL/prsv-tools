6e07729b13880fee10e7cc523e8cebb81c8c93c7
Nick Krabbenhoeft <nickkrabbenhoeft@nypl.org>
2022-11-04 14:23:37 -0400

    add test for package paths

--- tests/test_lint_er.py
+++ tests/test_lint_er.py
@@ -1,5 +1,24 @@
 import bin.lint_er as lint_er
 
+
+# Unit tests
+## Argument tests
+def test_accepts_paths(monkeypatch):
+    """Test that packages are returned as a list"""
+    paths = ['firstPath', 'secondPath']
+
+    monkeypatch.setattr(
+        'sys.argv', [
+            '../bin/lint_er.py',
+            '--package', paths[0], paths[1]
+        ]
+    )
+
+    args = lint_er.parse_args()
+    assert paths == args.packages
+
+
+# Functional tests
 def test_lint_valid_package():
     """Run entire script with valid ER"""
     lint_er.main()
