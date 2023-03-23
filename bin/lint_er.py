53b08117fc82b259a05f0d870e94d519e98117ce
hshiue <hilaryszuyinshiue@nypl.org>
2023-03-23 10:55:38 -0400

    fixing code style

--- bin/lint_er.py
+++ bin/lint_er.py
@@ -17,7 +17,8 @@ def _configure_logging(args):
                         format = "%(asctime)s - %(levelname)8s - %(message)s",
                         datefmt='%Y-%m-%d %H:%M:%S',
                         filename=log_fpath,
-                        encoding='utf-8')
+                        encoding='utf-8'
+                        )
 
 def parse_args() -> argparse.Namespace:
     """Validate and return command-line args"""
