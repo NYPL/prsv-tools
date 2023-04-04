11c332bc94716235d68014a3340564960d7baafe
hshiue <hilaryszuyinshiue@nypl.org>
2023-04-04 12:19:12 -0400

    add parse_args function

--- bin/flatten_er_metadata_folder.py
+++ bin/flatten_er_metadata_folder.py
@@ -6,6 +6,44 @@ import logging
 from pathlib import Path
 import re
 
+LOGGER = logging.getLogger(__name__)
+
+def parse_args() -> argparse.Namespace:
+    """Validate and return command-line args"""
+
+    def extant_dir(p):
+        path = Path(p)
+        if not path.is_dir():
+            raise argparse.ArgumentTypeError(
+                f'{path} does not exist'
+            )
+        return path
+
+    def list_of_paths(p):
+        path = extant_dir(p)
+        child_dirs = []
+        for child in path.iterdir():
+            if child.is_dir():
+                child_dirs.append(child)
+        return child_dirs
+
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        '--package',
+        type=extant_dir,
+        nargs='+',
+        dest='packages',
+        action='extend'
+    )
+    parser.add_argument(
+        '--directory',
+        type=list_of_paths,
+        dest='packages',
+        action='extend'
+    )
+
+    return parser.parse_args()
 
 def main():
     '''
@@ -15,6 +53,7 @@ def main():
         (a) if yes, move it / them up one level (naming convention will be checked with the linter)
         (b) if not, delete the folder (this function should be reusable)
     '''
+    args = parse_args()
 
 
 if __name__ == "__main__":
