917e82bf56505efe30a084f5e9f696891431a785
hshiue <hilaryszuyinshiue@nypl.org>
2023-04-04 11:51:04 -0400

    add instructions comment in main

--- bin/flatten_er_metadata_folder.py
+++ bin/flatten_er_metadata_folder.py
@@ -8,6 +8,13 @@ import re
 
 
 def main():
+    '''
+    1. Go through every package (e.g. M1234_ER_0001)
+    2. Get the path for submissionDocumentation
+    3. Check if the subissionDocumentation folder has any files
+        (a) if yes, move it / them up one level (naming convention will be checked with the linter)
+        (b) if not, delete the folder (this function should be reusable)
+    '''
 
 
 if __name__ == "__main__":
