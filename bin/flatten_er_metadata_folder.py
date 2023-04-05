7e0a55156c3e14a9f7f6e4534e05c3b4e3a44cb8
hshiue <hilaryszuyinshiue@nypl.org>
2023-04-05 17:15:27 -0400

    change when to get file list to avoid AttributeError

--- bin/flatten_er_metadata_folder.py
+++ bin/flatten_er_metadata_folder.py
@@ -78,8 +78,8 @@ def main():
     args = parse_args()
     for package in args.packages:
         subdoc_path = get_submissionDocumentation_path(package)
-        subdoc_file_ls = get_subdoc_file(subdoc_path)
         if subdoc_path:
+            subdoc_file_ls = get_subdoc_file(subdoc_path)
             if subdoc_file_ls:
                 move_subdoc_files_to_mdfolder(subdoc_file_ls)
             try:
