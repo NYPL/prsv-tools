982eb9606de74e4a630437476a5af0e118e87d8e
Nick Krabbenhoeft <nickkrabbenhoeft@nypl.org>
2023-08-09 15:54:23 -0400

    add coverage and mypy (testing)

--- noxfile.py
+++ noxfile.py
@@ -1,16 +1,51 @@
-import nox
+import sys
+from pathlib import Path
 
-python_versions=["3.10", "3.11"]
+import nox
+from nox_poetry import Session
+
+python_versions = ["3.11", "3.10"]
 nox.options.sessions = "lint", "tests"
 
-@nox.session(python=python_versions)
-def tests(session):
-    session.run("poetry", "install", external=True)
-    session.run("pytest")
 
-locations = "src", "tests", "noxfile.py"
+@nox.session(python=python_versions)
+def tests(session: Session) -> None:
+    """Run the test suite."""
+    session.install(".")
+    session.install("coverage[toml]", "pytest", "pygments")
+    try:
+        session.run("coverage", "run", "--parallel", "-m", "pytest", *session.posargs)
+    finally:
+        if session.interactive:
+            session.notify("coverage", posargs=[])
+
+
 @nox.session(python=python_versions)
 def lint(session):
-    args = session.posargs or locations
+    args = session.posargs or ["src", "tests", "noxfile.py"]
     session.install("flake8")
     session.run("flake8", *args)
+
+
+@nox.session(python=python_versions)
+def mypy(session: Session) -> None:
+    """Type-check using mypy."""
+    args = session.posargs or ["src", "tests"]
+    session.install(".")
+    session.install("mypy", "pytest")
+    session.run("mypy", *args)
+    if not session.posargs:
+        session.run("mypy", f"--python-executable={sys.executable}", "noxfile.py")
+
+
+@nox.session(python=python_versions[0])
+def coverage(session: Session) -> None:
+    """Produce the coverage report."""
+    args = session.posargs or ["report"]
+
+    session.install("coverage[toml]")
+
+    if not session.posargs and any(Path().glob(".coverage.*")):
+        session.run("coverage", "combine")
+
+    session.run("coverage", *args)
