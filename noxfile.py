f8b34b0f75f9c948e5146b99661d6c783220576c
Nick Krabbenhoeft <nickkrabbenhoeft@nypl.org>
2023-08-09 15:11:37 -0400

    add flake8 to nox

--- noxfile.py
+++ noxfile.py
@@ -1,7 +1,16 @@
 import nox
 
+python_versions=["3.10", "3.11"]
+nox.options.sessions = "lint", "tests"
 
-@nox.session(python=["3.10", "3.11"])
+@nox.session(python=python_versions)
 def tests(session):
     session.run("poetry", "install", external=True)
     session.run("pytest")
+
+locations = "src", "tests", "noxfile.py"
+@nox.session(python=python_versions)
+def lint(session):
+    args = session.posargs or locations
+    session.install("flake8")
+    session.run("flake8", *args)
