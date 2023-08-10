2335d9c57e149d6fc846ed65b4dbd0cb1eb1de3d
Nick Krabbenhoeft <nickkrabbenhoeft@nypl.org>
2023-08-10 13:21:24 -0400

    reorder nox a bit

--- noxfile.py
+++ noxfile.py
@@ -8,7 +8,22 @@ python_versions = ["3.11", "3.10"]
 nox.options.sessions = "format", "lint", "tests"
 
 
+@nox.session(python=python_versions[0])
+def format(session):
+    args = session.posargs or ["src", "tests"]
+    session.install("black", "isort")
+    session.run("black", *args)
+    session.run("isort", *args)
+
+
 @nox.session(python=python_versions)
+def lint(session):
+    args = session.posargs or ["src", "tests", "noxfile.py"]
+    session.install("flake8", "flake8-bugbear")
+    session.run("flake8", *args)
+
+
+nox.session(python=python_versions)
 def tests(session: Session) -> None:
     """Run the test suite."""
     session.install(".")
@@ -18,13 +33,6 @@ def tests(session: Session) -> None:
     finally:
         if session.interactive:
             session.notify("coverage", posargs=[])
-
-
-@nox.session(python=python_versions)
-def lint(session):
-    args = session.posargs or ["src", "tests", "noxfile.py"]
-    session.install("flake8", "flake8-bugbear")
-    session.run("flake8", *args)
 
 
 @nox.session(python=python_versions)
@@ -49,11 +57,3 @@ def coverage(session: Session) -> None:
         session.run("coverage", "combine")
 
     session.run("coverage", *args)
-
-
-@nox.session(python=python_versions[0])
-def format(session):
-    args = session.posargs or ["src", "tests"]
-    session.install("black", "isort")
-    session.run("black", *args)
-    session.run("isort", *args)
