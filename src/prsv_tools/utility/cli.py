44c32c7f385953a9131efaa95a3f09c0f69d5d42
Nick Krabbenhoeft <nickkrabbenhoeft@nypl.org>
2023-08-11 10:55:11 -0400

    type hint and improve module

--- src/prsv_tools/utility/cli.py
+++ src/prsv_tools/utility/cli.py
@@ -1,26 +1,9 @@
 import argparse
 from pathlib import Path
 
+
 class Parser(argparse.ArgumentParser):
-
-    def list_of_paths(self, p):
-            path = self.extant_dir(p)
-            child_dirs = []
-            for child in path.iterdir():
-                if child.is_dir():
-                    child_dirs.append(child)
-            return child_dirs
-
-
-    def extant_dir(self, p):
-            path = Path(p)
-            if not path.is_dir():
-                raise argparse.ArgumentTypeError(f"{path} does not exist")
-
-            return path
-
-
-    def add_instance(self):
+    def add_instance(self) -> None:
         self.add_argument(
             "--instance",
             "-i",
@@ -30,14 +13,47 @@ class Parser(argparse.ArgumentParser):
             help="Which Preservica instance to access",
         )
 
-
-    def add_package(self):
+    def add_package(self) -> None:
         self.add_argument(
-            "--package", type=self.extant_dir, nargs="+", dest="packages", action="extend"
+            "--package",
+            type=extant_dir,
+            nargs="+",
+            dest="packages",
+            action="extend",
+            help="path to a single package",
+        )
+
+    def add_packagedirectory(self) -> None:
+        self.add_argument(
+            "--directory",
+            type=list_of_paths,
+            dest="packages",
+            action="extend",
+            help="path to a directory of packages",
+        )
+
+    def add_logdirectory(self, dir: Path = ".") -> None:
+        self.add_argument(
+            "--log_folder",
+            type=extant_dir,
+            help="""Optional. Designate where to save the log file,
+            or it will be saved in current directory""",
+            default=dir,
         )
 
 
-    def add_packagedirectory(self):
-        self.add_argument(
-            "--directory", type=self.list_of_paths, dest="packages", action="extend"
-        )
+def list_of_paths(self, p: str) -> list[Path]:
+    path = self.extant_dir(p)
+    child_dirs = []
+    for child in path.iterdir():
+        if child.is_dir():
+            child_dirs.append(child)
+    return child_dirs
+
+
+def extant_dir(self, p: str) -> Path:
+    path = Path(p)
+    if not path.is_dir():
+        raise argparse.ArgumentTypeError(f"{path} does not exist")
+
+    return path
