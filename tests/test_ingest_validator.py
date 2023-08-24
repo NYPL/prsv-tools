fffa2c862d5b5b1b1573f4bbe893f5076c9c4714
hshiue <hilaryszuyinshiue@nypl.org>
2023-08-24 17:11:49 -0400

    add first test skeleton

--- tests/test_ingest_validator.py
+++ tests/test_ingest_validator.py
@@ -5,3 +5,6 @@ import json
 
 import prsv_tools.ingest.ingest_validator as ingest_validator
 
+def test_search_within_DigArch(accesstoken, collectionid, parentuuid):
+    """test that search_within_DigArch returns the 200 status code """
+    response = ingest_validator.search_within_DigArch(accesstoken, collectionid, parentuuid)
