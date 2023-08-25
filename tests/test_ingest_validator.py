999056d009645a364bc0662d673c1cb6d41eba08
hshiue <hilaryszuyinshiue@nypl.org>
2023-08-25 16:39:26 -0400

    add two assertions about the data from response

--- tests/test_ingest_validator.py
+++ tests/test_ingest_validator.py
@@ -14,13 +14,16 @@ content_endpoint = "https://nypl.preservica.com/api/content"
 entity_endpoint = "https://nypl.preservica.com/api/entity"
 test_digarch_uuid = "c0b9b47a-5552-4277-874e-092b3cc53af6"
 token = prsvapi.get_token("test-ingest")
-collectionid = "M848"
+collectionid = "M1126"
 
 # unit tests
 def test_content_searchwithin_so_endpoint():
     response = ingest_validator.search_within_DigArch(token, collectionid, test_digarch_uuid)
-
     assert response.status_code == 200
+
+    uuid_ls = ingest_validator.parse_structural_object_uuid(response)
+    assert type(uuid_ls) == list
+    assert len(uuid_ls) > 0
 
 
 
