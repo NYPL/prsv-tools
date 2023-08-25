df67977113b76d71c5a828cf2c8c4e6db7acee22
hshiue <hilaryszuyinshiue@nypl.org>
2023-08-25 15:56:55 -0400

    add first test

--- tests/test_ingest_validator.py
+++ tests/test_ingest_validator.py
@@ -1,10 +1,32 @@
 import pytest
+import requests
 from pathlib import Path
 import xml.etree.ElementTree as ET
 import json
 
+import prsv_tools.utility.api as prsvapi
 import prsv_tools.ingest.ingest_validator as ingest_validator
 
-def test_search_within_DigArch(accesstoken, collectionid, parentuuid):
-    """test that search_within_DigArch returns the 200 status code """
-    response = ingest_validator.search_within_DigArch(accesstoken, collectionid, parentuuid)
+
+# set up
+
+entity_endpoint = "https://nypl.preservica.com/api/entity"
+test_digarch_uuid = "c0b9b47a-5552-4277-874e-092b3cc53af6"
+token = prsvapi.get_token("test-ingest")
+
+# unit tests
+def test_entity_searchwithin_so_endpoint():
+    url = entity_endpoint + "/structural-objects/" + test_digarch_uuid
+    response = ingest_validator.get_api_results(token, url)
+
+    assert response.status_code == 200
+
+
+
+
+
+
+"""
+1. response code should be good (200)
+2. data structure should be as expected
+"""
