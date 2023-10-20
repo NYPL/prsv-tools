81d43ad21d86b12fe3d75150dfe0cb04f283a006
Nick Krabbenhoeft <nickkrabbenhoeft@nypl.org>
2023-10-20 17:04:56 -0400

    add more tests, refactor

--- tests/test_search.py
+++ tests/test_search.py
@@ -13,19 +13,65 @@ def json_response():
     return response
 
 
-def test_search_for_collection(json_response, mocker):
+COLL_SEARCH = [
+    ["M1234", "all", "", 2],
+    ["M1234", "", "all", 1],
+    ["M1234", "all", "all", 3],
+]
+
+
+@pytest.mark.parametrize("coll, er, ami, count", COLL_SEARCH)
+def test_search_for_collection(
+    json_response, mocker, coll: str, er: str, ami: str, count: int
+):
     mocker.patch(
         # api_call is from slow.py but imported to main.py
         "prsv_tools.utility.search.get_response",
         return_value=json_response,
     )
-    results = prsvsearch.search(coll_id="M1234")
-    print(results)
-    assert len(results) == 4
+    results = prsvsearch.search(coll_id=coll, er_id=er, ami_id=ami)
+
+    assert len(results) == count
 
 
-## start search session
-## return uuids based on results
-## report if nothing is found
+def filter_response(original: dict, object_index):
+    filtered = original.copy()
+    filtered["value"]["totalHits"] = 1
+    filtered["value"]["objectIds"] = [filtered["value"]["objectIds"][object_index]]
+    filtered["value"]["metadata"] = [filtered["value"]["metadata"][object_index]]
+    return filtered
 
-## should have option to return uuid or package?
+
+ITEM_SEARCH = [
+    ["M1234", "ER_2", "", 0, 1],
+    ["", "", "123456", 2, 1],
+]
+
+
+@pytest.mark.parametrize("coll, er, ami, index, count", ITEM_SEARCH)
+def test_search_for_item(
+    json_response: dict, mocker, coll: str, er: str, ami: str, index: int, count: int
+):
+    filtered_response = filter_response(json_response, index)
+    mocker.patch(
+        # api_call is from slow.py but imported to main.py
+        "prsv_tools.utility.search.get_response",
+        return_value=filtered_response,
+    )
+    results = prsvsearch.search(coll_id=coll, er_id=er, ami_id=ami)
+
+    assert len(results) == count
+
+
+def test_search_no_results(json_response, mocker):
+    json_response["value"]["totalHits"] = 0
+    mocker.patch(
+        # api_call is from slow.py but imported to main.py
+        "prsv_tools.utility.search.get_response",
+        return_value=json_response,
+    )
+
+    with pytest.raises(ValueError) as exc_info:
+        prsvsearch.search(coll_id="xxx", er_id="xxx", ami_id="xxx")
+
+    assert "expected one result, got none" in exc_info.value.args[0]
