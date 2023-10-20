045097ec167b931acfecc59e889f615722780f79
Nick Krabbenhoeft <nickkrabbenhoeft@nypl.org>
2023-10-20 12:02:51 -0400

    update cli module with ID args

--- tests/test_cli.py
+++ tests/test_cli.py
@@ -275,3 +275,143 @@ def test_reject_invalid_logdirectory(
     stderr = capsys.readouterr().err
 
     assert f"{str(nonexistant_dir)} is not a directory" in stderr
+
+
+# All tests for search related arguments
+SINGLE_ID_ARGS = {
+    'one_ami': {
+        '--ami': '123456'
+    },
+    'one_spec_object': {
+        '--object': '123456'
+    }
+}
+
+SET_IDS = ['--coll', '--acq']
+
+SET_ID_ARGS = {
+    'one_er': {
+        '--coll': 'M12345',
+        '--er': 'ER_1'
+    },
+    'all_coll_ami': {
+        '--coll': 'M12345',
+        '--ami': 'all'
+    },
+    'all_coll_er': {
+        '--er': 'all',
+        '--coll': 'M12345'
+    },
+    'all_coll': {
+        '--coll': 'M12345',
+        '--er': 'all',
+        '--ami': 'all'
+    },
+    'all_acq': {
+        '--object': 'all',
+        '--acq': '123456'
+    }
+}
+
+ALL_ID_ARGS = {**SINGLE_ID_ARGS, **SET_ID_ARGS}
+
+
+@pytest.mark.parametrize("scenario,ids", ALL_ID_ARGS.items())
+def test_accept_valid_id_sets(monkeypatch: pytest.MonkeyPatch, scenario: str, ids: dict):
+    cmd = ['script']
+    for id_type, id_value in ids.items():
+        cmd.extend([id_type, id_value])
+
+    monkeypatch.setattr("sys.argv", cmd)
+
+    fake_cli = prsvcli.Parser()
+    fake_cli.add_id_search()
+
+    args = fake_cli.parse_args()
+
+    for id_type, id_value in ids.items():
+        assert id_value in getattr(args, id_type[2:])
+
+
+@pytest.mark.parametrize("scenario,ids", ALL_ID_ARGS.items())
+def test_reject_invalid_ids(monkeypatch: pytest.MonkeyPatch, capsys: pytest.CaptureFixture, scenario: str, ids: dict):
+    cmd = ['script']
+
+    first_value = None
+    for id_type, id_value in ids.items():
+        if not first_value:
+            first_value = id_value
+        cmd.extend([id_type, f'@{id_value}@'])
+
+    monkeypatch.setattr('sys.argv', cmd)
+
+    fake_cli = prsvcli.Parser()
+    fake_cli.add_id_search()
+
+    with pytest.raises(SystemExit):
+        fake_cli.parse_args()
+
+    stderr = capsys.readouterr().err
+
+    assert f'@{first_value}@ does not match the expected' in stderr
+
+
+@pytest.mark.parametrize("scenario,ids", SET_ID_ARGS.items())
+def test_reject_missing_ids(monkeypatch: pytest.MonkeyPatch, capsys: pytest.CaptureFixture, scenario: str, ids: dict):
+    cmd = ['script']
+
+    for id_type, id_value in ids.items():
+        if not id_type in SET_IDS:
+            cmd.extend([id_type, id_value])
+        else:
+            required = id_type
+
+    monkeypatch.setattr('sys.argv', cmd)
+
+    fake_cli = prsvcli.Parser()
+    fake_cli.add_id_search()
+
+    with pytest.raises(SystemExit):
+        fake_cli.parse_args()
+
+    stderr = capsys.readouterr().err
+
+    assert f'the following arguments are required: {required}' in stderr
+
+
+@pytest.mark.parametrize("scenario,ids", ALL_ID_ARGS.items())
+def test_accept_multiple_ids(monkeypatch: pytest.MonkeyPatch, scenario: str, ids: dict):
+    cmd = ['script']
+    number_of_id_args = 6
+
+    for id_type, id_value in ids.items():
+        cmd.append(id_type)
+        if not id_type in SET_IDS:
+            cmd.extend([id_value] * number_of_id_args)
+            final_type = id_type
+            print(id_type)
+        else:
+            cmd.append(id_value)
+
+    monkeypatch.setattr('sys.argv', cmd)
+
+    fake_cli = prsvcli.Parser()
+    fake_cli.add_id_search()
+
+    args = fake_cli.parse_args()
+
+    assert number_of_id_args == len(getattr(args, final_type[2:]))
+
+def test_require_at_least_one_id(monkeypatch: pytest.MonkeyPatch, capsys: pytest.CaptureFixture):
+    cmd = ['script']
+
+    monkeypatch.setattr('sys.argv', cmd)
+
+    fake_cli = prsvcli.Parser()
+
+    with pytest.raises(SystemExit):
+        fake_cli.add_id_search()
+
+    stderr = capsys.readouterr().err
+
+    assert f'at least one ID argument is required' in stderr
