aa281d9eb1f3a052bdbc98175e1dac23827954f1
hshiue <hilaryszuyinshiue@nypl.org>
2023-12-12 15:48:35 -0500

    add validate_io_title function

--- src/prsv_tools/ingest/ingest_validator.py
+++ src/prsv_tools/ingest/ingest_validator.py
@@ -330,8 +330,16 @@ def get_contents_io_so(
             contents_element_so.extend(new_element_so)
     return contents_io, contents_element_so
 
+
+def validate_io_title(io_element: prsv_Information_Object) -> bool:
+    if isinstance(io_element.title, str):
+        return True
+    else:
+        logging.error(f"{io_element.title} is not a string")
+
+
 def validate_all_contents_element_io_conditions(io_element: prsv_Information_Object):
-    validate_io_title()
+    validate_io_title(io_element)
     validate_io_type()
     valid_sectag(io_element, "open")
     valid_ioCategory(io_element)
