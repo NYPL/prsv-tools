5adaf9ad1cd8614b9414f593a0464cd2a37d02dd
Nick Krabbenhoeft <nickkrabbenhoeft@nypl.org>
2024-09-20 13:21:08 -0400

    clean up retry

--- src/prsv_tools/ingest/retry_workflows.py
+++ src/prsv_tools/ingest/retry_workflows.py
@@ -23,45 +23,13 @@ def parse_args():
     return parser.parse_args()
 
 
-def get_api_results(accesstoken: str, url: str) -> requests.Response:
+def retry_stalled_workflows(token: str) -> None:
     headers = {
-        "Preservica-Access-Token": accesstoken,
+        "Preservica-Access-Token": token,
         "Content-Type": "application/xml",
     }
-    response = requests.request("GET", url, headers=headers)
-    return response
-
-
-def check_for_stalled_workflows(token: str) -> bool:
-    response = get_api_results(
-        token,
-        "https://nypl.preservica.com/sdb/rest/workflow/instances?state=Failed&type=Ingest&start=0&max=100",
-    )
-    root = ET.fromstring(response.text)
-    ns = {"": "http://workflow.preservica.com"}
-    if int(root.find("Count", namespaces=ns).text) > 0:
-        hung_workflows = [
-            instance for instance in root.iter("WorkflowInstance", namespaces=ns)
-        ]
-        for workflow in hung_workflows:
-            context = workflow.find("WorkflowContextName").text
-            started = (workflow.find("Started").text,)
-            step = workflow.find("CurrentStepName").text
-            group_id = workflow.find("WorkflowGroupID").text
-            LOGGER.info(
-                f"Stalled workflow found: {context} started at {started} in group {group_id} at step {step}"
-            )
-
-            LOGGER.info()
-        return True
-    else:
-        return False
-
-
-def retry_stalled_workflows(token: str) -> None:
-    response = get_api_results(
-        token, "https://nypl.preservica.com/sdb/rest/workflow/instances/retry"
-    )
+    url = "https://nypl.preservica.com/sdb/rest/workflow/instances/retry?workflowInstanceIds="
+    response = requests.request("POST", url, headers=headers)
 
     root = ET.fromstring(response.text)
     ns = {"": "http://workflow.preservica.com"}
